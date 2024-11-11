11f09b6a1e780f5db1cbf5f2161873dd24223531
jaeshim <sjm767@naver.com>
2024-11-11 22:22:53 +0900

    20241111

---
+++ hello-jpa/src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("hello");
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        // close
+        em.close();
+        emf.close();
+    }
+}
